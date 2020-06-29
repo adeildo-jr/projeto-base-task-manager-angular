814cdb1c85c86006883c2e5f7258c3264e00617d
Adeildo Júnior <adeildo@capitaldigitalaberto.com.br>
2020-06-29 00:29:15 -0300

    [Feature] Added getTask method

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -28,4 +28,8 @@ export class TaskService {
   public getImportantTaks(): Promise<Task[]> {
     return Promise.resolve(TASKS.slice(0, 3));
   }
+
+  public getTask(id: number): Promise <Task> {
+    return this.getTasks().then(tasks => tasks.find(task => task.id === id));
+  }
 }
